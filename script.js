9a02c0e08252ebbd7be5d27b571794486c3db9ec
Dhruv Patel <122099280+dhruvurgr8@users.noreply.github.com>
2024-02-16 17:19:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 // Your code here.
+let isDragging = false;
+    let startX;
+
+    const items = document.querySelector('.items');
+
+    items.addEventListener('mousedown', (e) => {
+      isDragging = true;
+      startX = e.clientX - items.offsetLeft;
+      items.classList.add('active');
+    });
+
+    items.addEventListener('mousemove', (e) => {
+      if (!isDragging) return;
+      e.preventDefault();
+      
+      const x = e.clientX - startX;
+      items.scrollLeft = -x;
+    });
+
+    items.addEventListener('mouseup', () => {
+      isDragging = false;
+      items.classList.remove('active');
+    });
+
+    items.addEventListener('mouseleave', () => {
+      isDragging = false;
+      items.classList.remove('active');
+    });
